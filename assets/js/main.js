a885ddcddb566977ba9f8ce9241aba151090926d
TheMitoSan <juliodogers@gmail.com>
2020-08-28 22:05:02 -0300

    update site

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/*
+	main.js
+*/
+window.onload = function(){
+	TMS_STARTUP();
+}
+function TMS_STARTUP(){
+	$('#MENU_CENTER').fadeIn({duration: 500, queue: false});
+	$('#DIV_TOOLS_BG').fadeIn({duration: 2000, queue: false});
+}
