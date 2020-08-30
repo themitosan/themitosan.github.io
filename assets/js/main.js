155edb8b7c636981ee38e0de99b8f5ffb8c85b3c
TheMitoSan <juliodogers@gmail.com>
2020-08-29 21:39:00 -0300

    lnks

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,5 @@ window.onload = function(){
 }
 function TMS_STARTUP(){
 	$('#MENU_CENTER').fadeIn({duration: 500, queue: false});
-	$('#DIV_TOOLS_BG').fadeIn({duration: 2000, queue: false});
+	$('#DIV_TOOLS_BG').fadeIn({duration: 2500, queue: false});
 }
